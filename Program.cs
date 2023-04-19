92b6b4f8ef3130e9482d79391c7ae563163e6277
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 16:57:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Double n1;
+            Double n2;
+            Double n3;
+            Double n4;
+            Double soma;
+            Double media;
+            Double Resultado;
+            Console.WriteLine("coloque o primeiro valor:");
+            n1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("coloque o segundo valor:");
+            n2 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("coloque o terceiro valor:");
+            n3 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("coloque o quarto valor:");
+            n4 = Double.Parse(Console.ReadLine());
+            soma = n1 + n2 + n3 + n4;
+            media = soma / 4;
+            Resultado = media;
+            Console.WriteLine("A media Aritmética é:");
+            Console.WriteLine(media);
+        }
+    }
+}
